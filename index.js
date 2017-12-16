f58753bbc168f4349dd2cbc731f472652f9b2333
hitakats <hitakats@yahoo-corp.jp>
2017-12-16 11:57:51 +0900

    feat(677): first implementation of command-validator

---
+++ index.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const Joi = require('joi');
+const SCHEMA_CONFIG = require('screwdriver-data-schema').config.command.schemaCommand;
+const Yaml = require('js-yaml');
+
+/**
+ * Loads the configuration from a stringified sd-command.yaml
+ * @method loadCommandSpecYaml
+ * @param  {String} yamlString Contents of sd-command.yaml
+ * @return {Promise}           Promise that resolves to the command as a config object
+ */
+function loadCommandSpecYaml(yamlString) {
+    return new Promise(resolve => resolve(Yaml.safeLoad(yamlString)));
+}
+
+/**
+ * Validate the command configuration
+ * @method validateCommand
+ * @param  {Object}         commandObj  Configuration object that represents the command
+ * @return {Promise}                    Promise that resolves to the passed-in config object
+ */
+function validateCommand(commandObj) {
+    return new Promise((resolve, reject) => {
+        Joi.validate(commandObj, SCHEMA_CONFIG, {
+            abortEarly: false
+        }, (err, data) => {
+            if (err) {
+                return reject(err);
+            }
+
+            return resolve(data);
+        });
+    });
+}
+
+/**
+ * Parses the configuration from a screwdriver-command-spec.yaml
+ * @method parseCommand
+ * @param  {String} yamlString Contents of screwdriver-command-spec.yaml
+ * @return {Promise}           Promise that rejects if the configuration cannot be parsed
+ *                             The promise will eventually resolve into:
+ *         {Object}   config
+ *         {Object}   config.command   The parsed command that was validated
+ *         {Object[]} config.errors    An array of objects related to validating
+ *                                     the given command
+ */
+function parseCommand(yamlString) {
+    return loadCommandSpecYaml(yamlString)
+        .then(configToValidate =>
+            validateCommand(configToValidate)
+                .then(commandConfiguration => ({
+                    errors: [],
+                    config: commandConfiguration
+                }), err => ({
+                    errors: err.details,
+                    config: configToValidate
+                }))
+        );
+}
+
+module.exports = parseCommand;
---
+++ test/index.test.js
@@ -0,0 +1,132 @@
+'use strict';
+
+const assert = require('chai').assert;
+const fs = require('fs');
+const hoek = require('hoek');
+const path = require('path');
+const validator = require('../index.js');
+
+const TEST_YAML_FOLDER = path.resolve(__dirname, 'data');
+const BINARY_COMMAND_PATH = path.resolve(TEST_YAML_FOLDER, 'valid_binary_command_spec.yaml');
+const DOCKER_COMMAND_PATH = path.resolve(TEST_YAML_FOLDER, 'valid_docker_command_spec.yaml');
+const HABITAT_COMMAND_PATH = path.resolve(TEST_YAML_FOLDER, 'valid_habitat_command_spec.yaml');
+const BAD_STRUCTURE_COMMAND_PATH = path.resolve(TEST_YAML_FOLDER, 'bad_structure_command.yaml');
+
+describe('index test', () => {
+    it('parses a valid binary command yaml', () => {
+        const yamlString = fs.readFileSync(BINARY_COMMAND_PATH);
+
+        return validator(yamlString)
+            .then((config) => {
+                assert.isObject(config);
+
+                assert.deepEqual(config, {
+                    errors: [],
+                    config: {
+                        description: 'this is sd-command of binary',
+                        maintainer: 'foo@bar.com',
+                        command: 'bar',
+                        namespace: 'foo',
+                        version: '1.0',
+                        format: 'binary',
+                        binary: {
+                            file: './foobar.sh'
+                        }
+                    }
+                });
+            });
+    });
+
+    it('parses a valid docker command yaml', () => {
+        const yamlString = fs.readFileSync(DOCKER_COMMAND_PATH);
+
+        return validator(yamlString)
+            .then((config) => {
+                assert.isObject(config);
+
+                assert.deepEqual(config, {
+                    errors: [],
+                    config: {
+                        description: 'this is sd-command of docker',
+                        maintainer: 'foo@bar.com',
+                        command: 'bar',
+                        namespace: 'foo',
+                        version: '1.0',
+                        format: 'docker',
+                        docker: {
+                            image: 'node:6',
+                            command: 'node'
+                        }
+                    }
+                });
+            });
+    });
+
+    it('parses a valid habitat command yaml', () => {
+        const yamlString = fs.readFileSync(HABITAT_COMMAND_PATH);
+
+        return validator(yamlString)
+            .then((config) => {
+                assert.isObject(config);
+
+                assert.deepEqual(config, {
+                    errors: [],
+                    config: {
+                        description: 'this is sd-command of habitat',
+                        maintainer: 'foo@bar.com',
+                        command: 'bar',
+                        namespace: 'foo',
+                        version: '1.0',
+                        format: 'habitat',
+                        habitat: {
+                            mode: 'remote',
+                            package: 'core/git/2.14.1',
+                            command: 'git'
+                        }
+                    }
+                });
+            });
+    });
+
+    it('validates a poorly structured command yaml', () => {
+        const yamlString = fs.readFileSync(BAD_STRUCTURE_COMMAND_PATH);
+
+        return validator(yamlString)
+            .then((result) => {
+                assert.deepEqual(result.config, {
+                    maintainer: 'foo@bar.com',
+                    command: 'bar',
+                    namespace: 'foo',
+                    version: '1.0',
+                    format: 'docker',
+                    habitat: {
+                        mode: 'remote',
+                        package: 'core/git',
+                        command: 'git'
+                    }
+                });
+                assert.strictEqual(result.errors.length, 2);
+
+                // check required description
+                const missingField = hoek.reach(result.config, result.errors[0].path);
+
+                assert.strictEqual(result.errors[0].message, '"description" is required');
+                assert.isUndefined(missingField);
+
+                // check required docker specified in format
+                const missingField2 = hoek.reach(result.config, result.errors[1].path);
+
+                assert.strictEqual(result.errors[1].message, '"docker" is required');
+                assert.isUndefined(missingField2);
+            }, assert.fail);
+    });
+
+    it('throws when parsing incorrectly formatted yaml', () => {
+        const yamlString = 'main: :';
+
+        return validator(yamlString)
+            .then(assert.fail, (err) => {
+                assert.match(err, /YAMLException/);
+            });
+    });
+});
